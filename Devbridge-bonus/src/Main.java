899efb834a37ecc3c4d13b55a02361145909873b
Benas <benas.lukosius@ktu.edu>
2023-09-13 19:33:28 +0300

    Added comments to explain how the program works

--- Devbridge-bonus/src/Main.java
+++ Devbridge-bonus/src/Main.java
@@ -5,13 +5,13 @@ public class Main {
     public static void printBonusDatesBetween(int fromYear, int toYear) {
         if(toYear > 9999 || fromYear > toYear || fromYear<1000) return;
         for (int i=fromYear; i<=toYear; i++) {
-            boolean magic = false;
+            boolean magic = false;                  //If this boolean is true in the end of the cycle, the year is a palindrome
             int month = (i%10)*10 + (i/10%10);
             int day = i/100%10*10 + i/1000;
 
-            if(month==2 && day<29) magic=true;
+            if(month==2 && day<29) magic=true;      //Special case for February
 
-            switch (month%2) {
+            switch (month%2) {                      //Checking whether a month has 30 or 31 days
                 case 0:
                     if(month<13 && day<31) magic=true;
                     break;
@@ -19,7 +19,7 @@ public class Main {
                     if(month<13 && day<32) magic=true;
                     break;
             }
-            if(day<1 || month<1) magic=false;
+            if(day<1 || month<1) magic=false;       //If either day or month is zero, don't print
             if(magic) System.out.println(i + "-" + String.format("%02d", month) + "-" + String.format("%02d", day));
         }
     }
